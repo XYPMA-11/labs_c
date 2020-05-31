7089081e1aa2ec24a281372a77c5cbfc6ac80017
XYPMA-11 <danilkop2000@mail.ru>
2020-05-31 16:41:18 +0300

    chahged lab_10 matrix

--- lab_10/Main.cpp
+++ lab_10/Main.cpp
@@ -13,13 +13,13 @@ int main()
 	Matrix2D m2a, m2b;
 	Matrix3D m3a, m3b;
 
-	cout << "Начальная матрица 2х2: " << endl << m2a << endl;
+	cout << "Начальные матрица 2х2: " << endl << m2a << endl << m2b << endl;
 	m2a *= multiplier;
 	cout << "Умножение матрицы на " << multiplier << ": " << endl << m2a << endl;
 	m2a += m2b;
 	cout << "Сложение начальной и умноженной матрицы: " << endl << m2a << endl;
 
-	cout << "Начальная матрица 3х3: " << endl << m3a << endl;
+	cout << "Начальные матрица 3х3: " << endl << m3a << endl << m3b << endl;
 	m3a *= multiplier;
 	cout << "Умножение матрицы на " << multiplier << ": " << endl << m3a << endl;
 	m3a += m3b;
